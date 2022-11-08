28c07a8dc57d6929ed97b31594f10a5f0e5fbe2f
Del Russo, Anthony <anthony.del.russo@merck.com>
2022-11-08 08:23:09 -0500

    Add VFN approvals tests

--- tests/unit/test_approvals.py
+++ tests/unit/test_approvals.py
@@ -1,4 +1,4 @@
-from src.domain.approvals import VpackApprovals
+from src.domain.approvals import VpackApprovals, VFNApprovals
 import datetime as dt
 
 
@@ -43,5 +43,38 @@ def test_vpack_approvals_return_false(sku, asset):
     assert approvals.get_approval(sku, asset) is False
 
 
-def test_vfn_approvals_return_true():
-    pass
+def test_vfn_approvals_return_true(sku, asset):
+    approvals = VFNApprovals(
+        {
+            ("Haarlem-V11", "LA", "SYRINGE", "Gardasil 9", "Peru"): (
+                dt.datetime(year=2022, month=1, day=1),
+                dt.datetime(year=2031, month=1, day=1),
+            )
+        }
+    )
+
+    assert approvals.get_approval(sku, asset) is True
+
+    approvals = VFNApprovals(
+        {
+            ("Haarlem-V11", "LA", "SYRINGE", "Gardasil 9", "All"): (
+                dt.datetime(year=2022, month=1, day=1),
+                dt.datetime(year=2031, month=1, day=1),
+            )
+        }
+    )
+
+    assert approvals.get_approval(sku, asset) is True
+
+
+def test_vfn_approvals_return_false(sku, asset):
+    approvals = VFNApprovals(
+        {
+            ("Haarlem-V11", "US", "SYRINGE", "Gardasil 9", "All"): (
+                dt.datetime(year=2022, month=1, day=1),
+                dt.datetime(year=2031, month=1, day=1),
+            )
+        }
+    )
+
+    assert approvals.get_approval(sku, asset) is False
